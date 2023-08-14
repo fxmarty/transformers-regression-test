e41cd534c7ba56fcebb94df690b6532e10002d4c
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-08-14 12:12:38 +0200

    fix device set

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -8,7 +8,23 @@ echo "COMMIT_DATE_GMT: ${COMMIT_DATE_GMT}"
 echo "All yaml to use:"
 cat yaml_list.txt
 
+echo "nvidia-smi:"
+nvidia-smi
+
 while read YAML_FILE_NAME; do
     echo "Running optimum-benchmark for: ${YAML_FILE_NAME}.yml"
+    if [ "cpu" == *$YAML_FILE_NAME* ]; then
+        export CUDA_VISIBLE_DEVICES=""
+    elif [ "1gpu" == *$YAML_FILE_NAME* ]; then
+        export CUDA_VISIBLE_DEVICES="0"
+    elif [ "2gpu" == *$YAML_FILE_NAME* ]; then
+        export CUDA_VISIBLE_DEVICES="0,1"
+    elif [ "4gpu" == *$YAML_FILE_NAME* ]; then
+        export CUDA_VISIBLE_DEVICES="0,1,2,3"
+    else
+        echo "ERROR: could not set CUDA_VISIBLE_DEVICES"
+        exit 1
+    fi
+    echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
     optimum-benchmark --config-dir configs --config-name $YAML_FILE_NAME --multirun
 done <yaml_list.txt
